811c6703e8a1a6efdf39b00082a8f76c9b17525a
Paul Mederos <pvm@enchant.co>
2012-08-27 23:55:33 -0400

    Added subpage routes.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,21 @@ class PagesController < ApplicationController
 	def home
 
 	end
+
+	def what_is
+
+	end
+
+	def get_involved
+
+	end
+
+	def get_inspired
+
+	end
+
+	def meet_us
+
+	end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 RebootAmerica::Application.routes.draw do
 
+  match '/what-is' => 'pages#what_is'
+  match '/get-involved' => 'pages#get_involved'
+  match '/get-inspired' => 'pages#get_inspired'
+  match '/meet-us' => 'pages#meet_us'
+
   match 'user/edit' => 'users#edit', :as => :edit_current_user
-
   match 'signup' => 'users#new', :as => :signup
-
   match 'logout' => 'sessions#destroy', :as => :logout
-
   match 'login' => 'sessions#new', :as => :login
 
   resources :sessions
-
   resources :users
 
   root :to => "pages#home"
