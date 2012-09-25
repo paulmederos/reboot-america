9fdd6caf08db233d02408024a6832a1b993cf0d2
Paul Mederos <pvm@enchant.co>
2012-09-24 23:14:04 -0400

    Added slider images. Bxslider used for content fading. Also added images for Citizens, Startups, Corporations.

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,9 @@
+ $(document).ready(function(){
+    $('#slider').bxSlider({
+    	mode: 'fade',
+    	randomStart: 'true',
+    	auto: 'true',
+    	speed: '100',
+    	pause: '6000'
+    });
+  });
